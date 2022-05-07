6ae17fd722d4831df73e0c2465ede23885cf5e45
Basirul Billah <basirulbillah@gmail.com>
2022-05-07 11:21:03 +0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
+import Blog from './Pages/Blog/Blog';
+import Home from './Pages/Home/Home';
+import Inventory from './Pages/Inventory/Inventory';
+import LogIn from './Pages/LogIn/LogIn';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/login' element={<LogIn></LogIn>}></Route>
+      </Routes>
+
     </div>
   );
 }
---
+++ src/Pages/LogIn/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogIn = () => {
+    return (
+        <div>
+            Log in
+        </div>
+    );
+};
+
+export default LogIn;
