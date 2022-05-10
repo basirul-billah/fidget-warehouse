d4ea547e65eab0689f7e7a165def553e29694179
Basirul Billah <basirulbillah@gmail.com>
2022-05-10 10:23:23 +0600

    added not footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import RequireAuth from './Components/RequireAuth/RequireAuth';
 import Blog from './Pages/Blog/Blog';
@@ -11,21 +12,24 @@ import Register from './Pages/Register/Register';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/inventory' element={
-          <RequireAuth>
-            <Inventory></Inventory>
-          </RequireAuth>
-        }></Route>
-        <Route path='/blog' element={<Blog></Blog>}></Route>
-        <Route path='/login' element={<LogIn></LogIn>}></Route>
-        <Route path='/register' element={<Register></Register>}></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
-      </Routes>
+    <div className="App relative min-h-screen">
+      <div className="pb-14">
+        <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/home' element={<Home></Home>}></Route>
+          <Route path='/inventory' element={
+            <RequireAuth>
+              <Inventory></Inventory>
+            </RequireAuth>
+          }></Route>
+          <Route path='/blog' element={<Blog></Blog>}></Route>
+          <Route path='/login' element={<LogIn></LogIn>}></Route>
+          <Route path='/register' element={<Register></Register>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
+        </Routes>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='absolute bottom-0 w-screen p-4 bg-[#15263F] text-gray-500 text-center'>
+            © 2022 Fidget Warehouse™. 
+            <span> </span>
+            All Rights Reserved.
+        </div>
+    );
+};
+
+export default Footer;
